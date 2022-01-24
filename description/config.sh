b6813b644841ce6e487df9d9d67f72422e797045
Petr Velička <petrvelicka@tuta.io>
2022-01-24 12:41:32 +0100

    fix display manager

--- description/config.sh
+++ description/config.sh
@@ -35,7 +35,7 @@ suseSetupProduct
 # Activate services
 #--------------------------------------
 baseInsertService sshd
-baseInsertService lightdm
+baseInsertService sddm
 
 #======================================
 # Setup default target, multi-user
